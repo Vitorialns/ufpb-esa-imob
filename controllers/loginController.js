4ccd7ced014937427ba604b9d6688c7b2a7a17a9
Adyson Lucas <adyson.lucas@dce.ufpb.br>
2018-05-28 21:31:22 -0300

    Add files via upload

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,10 +4,18 @@ const electron = require('electron');
      event.preventDefault();
      const login = document.getElementById('login').value;
      const password = document.getElementById('password').value;
+     const message = document.getElementById('message');
+     message.innerHTML = '*';
      ipcRenderer.send('adm:login', login, password);
+
+
    });
 
    ipcRenderer.on('adm:loginfail', () => {
+     const login = document.getElementById('login').value;
+     const password = document.getElementById('password').value;
      const message = document.getElementById('message');
+     login.value = '';
+     password.value = '';
      message.innerHTML = 'Login ou senha incorretos.';
    });
